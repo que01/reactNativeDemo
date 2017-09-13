30d7997e5f5a50e601e80d4fd135ede324214a32
que01 <que01@foxmail.com>
2017-09-13 15:59:49 +0800

    初步完善home

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView, View } from 'react-native'
+import { ScrollView, Text, KeyboardAvoidingView, View, TouchableOpacity, Image } from 'react-native'
 import { connect } from 'react-redux'
 import Tabbar from '../Components/TabBar'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
@@ -7,7 +7,41 @@ import Tabbar from '../Components/TabBar'
 
 // Styles
 import styles from './Styles/HomeScreenStyle'
+const menus = [
+  {
+    "name": '美食1',
+    "icon": 'https://img.la/333/fff/60x60'
+  },
+  {
+    "name": '美食2',
+    "icon": 'https://img.la/333/fff/60x60'
+  },
+  {
+    "name": '美食3',
+    "icon": 'https://img.la/333/fff/60x60'
+  },
+  {
+    "name": '美食4',
+    "icon": 'https://img.la/333/fff/60x60'
+  },
+  {
+    "name": '美食5',
+    "icon": 'https://img.la/333/fff/60x60'
+  },
+  {
+    "name": '美食6',
+    "icon": 'https://img.la/333/fff/60x60'
+  },
+  {
+    "name": '美食7',
+    "icon": 'https://img.la/333/fff/60x60'
+  },
+  {
+    "name": '美食8',
+    "icon": 'https://img.la/333/fff/60x60'
+  },
 
+];
 class HomeScreen extends Component {
   render () {
     const { navigate } = this.props.navigation
@@ -15,7 +49,16 @@ class HomeScreen extends Component {
       <View style={styles.container}>
         <ScrollView >
           <KeyboardAvoidingView behavior='position'>
-            <Text>HomeScreen</Text>
+            <View style={styles.menuItemWrapper}>
+              {
+                menus.map(({name, icon},i)=>(
+                  <TouchableOpacity style={styles.menuItem} key={`icon-${i}`}>
+                    <Image source={{uri: icon}} style={styles.menuItemIcon}/>
+                    <Text>{name}</Text>
+                  </TouchableOpacity>
+                ))
+              }
+            </View>
           </KeyboardAvoidingView>
         </ScrollView>
         <Tabbar navTo={navigate.bind(this)} />
--- App/Containers/Styles/HomeScreenStyle.js
+++ App/Containers/Styles/HomeScreenStyle.js
@@ -1,9 +1,29 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Dimensions } from 'react-native'
 import { ApplicationStyles } from '../../Themes/'
+const { width, height } = Dimensions.get('window')
 
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   container: {
     flex: 1
+  },
+  menuItemWrapper:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    zIndex: 1
+  },
+  menuItem: {
+    flex: 1,
+    minWidth: width/4,
+    height: width/4 + 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  menuItemIcon: {
+    height: (width/4)*0.8,
+    width: (width/4)*0.8,
+    borderRadius: (width/4)*0.4,
+    marginBottom: 5
   }
 })
